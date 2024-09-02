3ba2d44d1ffacfbbb51bd5abb909809e1022eed4
aboodlibda <aboodlibda@gmail.com>
2024-09-02 11:26:26 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -182,7 +182,7 @@
                                     <p class="text-xs text-gray-400"></p>
                                 </div><button
                                     class="bg-primary hover:bg-primary-light active:bg-primary-dark ring-primary text-primary-contrast focus:ring-1 ring-offset-2 ring-offset-body relative inline-flex items-center justify-center gap-1 overflow-hidden px-4 py-2 text-center transition disabled:bg-input/10 disabled:text-text/40 focus:outline-none disabled:cursor-not-allowed rounded-md w-full max-w-xs"
-                                    tabindex="-1"> </button>
+                                    tabindex="-1"> احجز التذاكر</button>
                             </div>
                             <div
                                 class="absolute inset-0 flex flex-col justify-between bg-gradient-to-b from-black/5 via-black/10 to-black/95 to-90% p-4 md:hidden">
